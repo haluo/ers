4fd9148dbc19ffef322f384ceee3e7c40606a0fb
朱峰 <zhufeng@autohome.com.cn>
2021-01-01 16:43:40 +0800

    vec

--- e_vector/src/main.rs
+++ e_vector/src/main.rs
@@ -5,6 +5,37 @@ fn main() {
     println!("Hello, world!");
     
     let v1 = vec![1,2,3];
+    
+    //下标获取
     let one:&i32 = &v1[0];
-    print!("one = {}",*one)
+    println!("one = {}",*one);
+    //match操作(推荐)
+    match v1.get(1){
+        Some(value) => println!("value={}",value),
+        _=>println!("None")
+    }
+
+    let mut v2:Vec<i32> = Vec::new();
+    v2.push(1);
+    v2.push(2);
+    v2.push(3);
+
+    //不可变遍历
+    for i in &v2{
+        println!("i = {}",i);
+    }
+    //可变遍历
+    for i in &mut v2{
+        *i+=1;
+        println!("i = {}",i);
+    }
+
+    //通过枚举使Vec存储多种类型
+    enum Context{
+        Text(String),
+        Float(f32),
+        Int(i32),
+    }
+    let c = vec![Context::Text(String::from("a string")),Context::Int(-1)];
+    
 }
