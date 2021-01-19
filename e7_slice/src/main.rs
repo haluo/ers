20674d16c82b75e4a2fedce6ba8d960bb27ccad3
zhufeng <zhufeng3612@163.com>
2021-01-19 12:37:02 +0800

    slice

--- e7_slice/src/main.rs
+++ e7_slice/src/main.rs
@@ -2,5 +2,26 @@
 //2.字面值就是slice
 //3.其他类型的slice
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+    let h = &s[0..5];
+    println!("h = {}",h);
+    let h = &s[0..=4];
+    println!("h = {}",h);
+    let h = &s[..=4];
+    println!("h = {}",h);
+    let h = &s[6..];
+    println!("h = {}",h);
+    let h = &s[..];
+    println!("h = {}",h);
+    // let ss = String::from("你好");
+    // let  h = &ss[0..1];
+    let s3 = "h";// &str 是不可变引用
+    println!("-----------------------");
+
+
+    let a = [1,2,3,4];
+    let sss = &a[1..3];
+    println!("sss[0] ={} ",sss[0]);
+    println!("sss[1] ={} ",sss[1]);
+    
 }
