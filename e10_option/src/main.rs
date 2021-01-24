4d30ae4907fd02605eb17ab82d44cd63f54a7117
朱峰 <zhufeng@autohome.com.cn>
2021-01-24 22:13:04 +0800

    option

--- e10_option/src/main.rs
+++ e10_option/src/main.rs
@@ -10,5 +10,35 @@ fn main() {
 
     let x:i32 = 5;
     let y:Option<i32> = Some(5);
+    let mut tmp = 0;
+    match y{
+        Some(i )=> {tmp = i},
+        Nome => {tmp = 0},    
+    }
+    let sum = x +tmp;
+    println!("sum = {}",sum);
 
+    let reslut = plus_one(y);
+    match reslut {
+        Some(i) => println!("result = {}",i),
+        None => println!("result is null"),
+    }
+
+    //if let
+    if let Some(v) = plus_one(y){
+        println!("v = {}",v);
+    } 
+
+    if let Some(v) = plus_one(y){
+        println!("v = {}",v);
+    } else {
+        println!("v  is null");     
+    }
+}
+
+fn  plus_one(x:Option<i32>)->Option<i32>{
+    match x {
+        None =>None,
+        Some(i) =>Some(i+1),
+    }
 }
