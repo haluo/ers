72b448763c11deeb0ee2169902f13da0280050df
朱峰 <zhufeng@autohome.com.cn>
2021-01-26 22:39:47 +0800

    crate

--- e14_crate2/src/main.rs
+++ e14_crate2/src/main.rs
@@ -1,6 +1,7 @@
 use mylib::factory::produce_a;
 use mylib::factory::produce_b as b;
 //use mylib::factory::*;
+//!!! 注意配置文件增加依赖  mylib = {path="./mylib"}
 
 fn main() {
     mylib::factory::produce_a::produce_a();
